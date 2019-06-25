be0cdf1dc724427bccab6ef92eef69386f0d92a2
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2019-06-26 00:17:07 +0300

    working proxy

--- mainbotfile.py
+++ mainbotfile.py
@@ -1,14 +1,15 @@
 import telebot
 token = "691808572:AAGxVfNlK7EzwMHjxm0P0zY0s99pDat3wMY"
+telebot.apihelper.proxy = {'https': 'socks5://geek:socks@t.geekclass.ru:7777'}
 bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == "привет":
-        bot.send_message(message.form_user.id, "Ну привет")
+        bot.send_message(message.chat.id, "Ну привет")
     elif message.text == "/help":
-        bot.send_message(message.form_user, "Ботик для Валентина, напиши привет.")
+        bot.send_message(message.chat.id, "Ботик для Валентина, напиши привет.")
     else:
-        bot.send_message(message.from_user.id, "Бро, напиши /help.")
+        bot.send_message(message.chat.id, "Бро, напиши /help.")
 
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, timeout=60)
